42d876263d367cc9f70addf497bc9fd1f34816e5
nicolewallace09 <nicole.elisaw@gmail.com>
2020-03-29 12:59:32 -0700

    restart enemyHealth

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,7 +17,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy robot is alive 
+    while(enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
 // if player choses to fight, then fight
@@ -26,7 +27,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount of set in the playerAttack variable 
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has" + enemyHealth + " health remaining."); 
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."); 
 
     //check enemy's health 
     if (enemyHealth <=0) { 
@@ -46,6 +47,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     } else {
         window.alert(playerName + " still has " + playerHealth + " health left. ");
     }
+        
 
     // if player choses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
@@ -71,10 +73,14 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
 }
 
+}
+
 console.log(enemyNames.length);
 
 
 for (var i = 0; i < enemyNames.length; i++) {
-   fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 
 }
